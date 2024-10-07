fd382f9d38c94506787cccae155d82c6eab023d0
XYHK <1342103593@qq.com>
2024-10-07 17:14:56 +0800

    具体实现原理

--- LinkList/deleteDuplicates.py
+++ LinkList/deleteDuplicates.py
@@ -1,6 +1,10 @@
 from typing import Optional
 from ListNode import ListNode
 
+# 使用两个指针，pre指向前驱，cur指向当前节点
+# cur指向的节点如果和后续值相同，则往后遍历
+# 直到找到val相同的结点的最后一个，然后让pre.next = cur.next cur = cur.next
+
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
         dummy = pre = cur = ListNode(next=head)
