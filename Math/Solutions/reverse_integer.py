717a5714d1985895f43e94c19db4a55929a7076a
XYHK <1342103593@qq.com>
2024-12-16 20:56:58 +0800

    10.19

--- Math/Solutions/reverse_integer.py
+++ Math/Solutions/reverse_integer.py
@@ -24,6 +24,8 @@ if __name__ == "__main__":
     res01 = reverse(-123)
     res02 = reverse(-1289593023)
     res03 = reverse(20020419)
+    res04 = reverse(20021024)
     print(f"res01 = {res01}")
     print(f"res02 = {res02}")
     print(f"res03 = {res03}")
+    print(f"res04 = {res04}")
